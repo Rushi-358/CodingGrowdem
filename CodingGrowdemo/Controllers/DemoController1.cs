123148d120a6f98cbc64475c5985a89de0adfb8c
Rushi-358 <koustubhl358@gmail.com>
2025-12-30 14:59:27 +0530

    Demo

---
+++ CodingGrowdemo/Controllers/DemoController1.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CodingGrowdemo.Controllers
+{
+    public class DemoController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            Console.WriteLine("DemoController1 Index action called.");
+            return View();
+        }
+    }
+}
